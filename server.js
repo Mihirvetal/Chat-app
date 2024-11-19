aec31cdc7917dcbb5f478ef6b7eb4b49ef757f7f
Mihir Vetal <151329052+Mihirvetal@users.noreply.github.com>
2024-11-19 15:43:47 +0530

    Done the project

--- server.js
+++ server.js
@@ -1,18 +1,27 @@
-const express = require("express")
-const http = require("http")
-const socketIo = require('socket.io')
+const express = require("express");
+const http = require("http");
+const socketIo = require('socket.io');
 
-const PORT = 3001
+const PORT = 3001;
 
-
-const app = express()
+const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
+app.use(express.static('public'));
 
+io.on('connection', (socket) => {
+    console.log('A user connected');
 
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
 
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
 
-app.listen(PORT , () => {
-    console.log("server Started");
-})
+server.listen(PORT, () => {
+    console.log(`Server started on http://localhost:${PORT}`);
+});
